33d0ee603c2ed54497482858e0e20bc214471e6f
Jad-Kreit29 <jkrei015@uottawa.ca>
2025-05-25 16:17:11 -0400

    Finished 404 Page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FaWrench, FaExclamationTriangle } from 'react-icons/fa'
 
 const NotFoundPage = () => {
   return (
-    <div>
 
-        <h1 className='flex text-7xl font-extrabold justify-center h-screen'>UNDER CONSUTRCTION!</h1>
+    <>
 
-    </div>
+      <div className='flex h-screen justify-center items-center space-x-5'>
+
+        <FaExclamationTriangle className='text-amber-400 text-7xl' />
+        <h1 className='text-7xl text-[#01161E] font-extrabold underline underline-offset-4'>UNDER CONSUTRCTION!</h1>
+        <FaWrench className='text-amber-400 text-7xl' />
+
+      </div>
+    
+    </>
+
+    
+
   )
 }
 
